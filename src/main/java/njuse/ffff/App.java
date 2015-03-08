c0d3688c9c920354770edaebdbfc5e53c68d0f4e
troyeagle <979206913@qq.com>
2015-03-08 22:51:40 +0800

    TestCommit3.8
    Just modify if Git can properly work

--- src/main/java/njuse/ffff/App.java
+++ src/main/java/njuse/ffff/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    //Modifier
+    public static void method1(){
+    	System.out.println("Diaoziheng13150084has modified its github");
+    }
 }
---
+++ src/main/java/njuse/ffff/TestOfGithub.java
@@ -0,0 +1,5 @@
+package njuse.ffff;
+
+public class TestOfGithub {
+	//This is only a class to test adding files
+}
