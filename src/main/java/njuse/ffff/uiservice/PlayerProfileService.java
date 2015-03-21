ff69390954f8c84aa562b8fb407c5dc720ececb1
Li <lwpqq000@gmail.com>
2015-03-21 15:57:56 +0900

    更新部分接口名称
    PlayerDetailService->PlayerProfileService
    TeamDetailService->TeamProfileService

---
+++ src/main/java/njuse/ffff/uiservice/PlayerProfileService.java
@@ -0,0 +1,31 @@
+package njuse.ffff.uiservice;
+
+import java.awt.Image;
+
+/**
+ * 提供球员简介的访问和修改方法
+ * 
+ * @author Li
+ *
+ */
+public interface PlayerProfileService {
+	/**
+	 * 设置球员简介
+	 * 
+	 * @param name
+	 *            球员名
+	 * @param position
+	 *            球员的位置
+	 * @param properties
+	 *            球员的属性
+	 */
+	void setProfile(String name, String position, String[][] properties);
+
+	/**
+	 * 设置球员的照片
+	 * 
+	 * @param photo
+	 */
+	void setPhoto(Image photo);
+
+}
---
+++ src/main/java/njuse/ffff/uiservice/TeamProfileService.java
@@ -0,0 +1,37 @@
+package njuse.ffff.uiservice;
+
+import java.awt.Image;
+
+/**
+ * 提供访问球队简介界面数据的接口
+ * 
+ * @author Li
+ *
+ */
+public interface TeamProfileService {
+
+	/**
+	 * 设置球队简介
+	 * 
+	 * @param name
+	 *            球队名
+	 * @param properties
+	 *            球队的属性
+	 */
+	void setProfile(String name, String[][] properties);
+
+	/**
+	 * 设置球队的队徽
+	 * 
+	 * @param icon
+	 */
+	void setIcon(Image icon);
+
+	/**
+	 * 设置球队的照片
+	 * 
+	 * @param photo
+	 */
+	void setPhoto(Image photo);
+
+}
