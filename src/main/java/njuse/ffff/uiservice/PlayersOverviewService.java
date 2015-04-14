8bedbee53e6ae65a271b95f92acb70392a56571a
Yoctillion <lwpqq000@gmail.com>
2015-04-14 20:49:10 +0800

    接口更新v3
    忘记改的

--- src/main/java/njuse/ffff/uiservice/PlayersOverviewService.java
+++ src/main/java/njuse/ffff/uiservice/PlayersOverviewService.java
@@ -1,7 +1,5 @@
 package njuse.ffff.uiservice;
 
-import javax.swing.ImageIcon;
-
 /**
  * 提供球员一览界面访问和修改方法
  * 
@@ -11,20 +9,10 @@ import javax.swing.ImageIcon;
 public interface PlayersOverviewService {
 
 	/**
-	 * 设置所有球员的属性及数据
+	 * 设置所有球员的数据
 	 * 
-	 * @param properties
 	 * @param values
 	 */
-	void setPlayersInfo(String[] properties, Object[][] values);
+	void setPlayersInfo(Object[][] values);
 
-	/**
-	 * 设置球员的头像
-	 * 
-	 * @param names
-	 *            球员姓名数组
-	 * @param photos
-	 *            头像数组
-	 */
-	void setPlayersPhoto(String[] names, ImageIcon[] photos);
 }
--- src/main/java/njuse/ffff/uiservice/TeamsOverviewService.java
+++ src/main/java/njuse/ffff/uiservice/TeamsOverviewService.java
@@ -1,6 +1,5 @@
 package njuse.ffff.uiservice;
 
-import javax.swing.ImageIcon;
 
 /**
  * 提供球队一览界面访问和修改方法
@@ -13,18 +12,7 @@ public interface TeamsOverviewService {
 	/**
 	 * 设置所有球队的信息
 	 * 
-	 * @param properties
 	 * @param values
 	 */
-	void setTeamsInfo(String[] columns, Object[][] values);
-
-	/**
-	 * 设置球队的队徽
-	 * 
-	 * @param names
-	 *            队名数组，对应values中的name项（如果存在）
-	 * @param icons
-	 *            队徽图像数组
-	 */
-	void setTeamsIcon(String[] names, ImageIcon[] icons);
+	void setTeamsInfo(Object[][] values);
 }
