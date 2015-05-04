23746d2dcf84b5314f91536819cedd75d27bad7e
troyeagle <979206913@qq.com>
2015-05-04 18:33:59 +0800

    一个Stub

---
+++ src/main/java/njuse/ffff/Console.java
@@ -0,0 +1,13 @@
+package njuse.ffff;
+
+import java.io.PrintStream;
+
+import test.data.*;
+
+public class Console {
+	public static void execute(PrintStream out,String[] args){
+		//TODO hehe
+
+
+	}
+}
