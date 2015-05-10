1d5bf4cd32ed0620cf504a9d046b5045f82d12de
troyeagle <979206913@qq.com>
2015-05-10 23:13:48 +0800

    修正当前赛季的自动生成

--- src/main/java/functionTest/TestConsole.java
+++ src/main/java/functionTest/TestConsole.java
@@ -8,15 +8,15 @@ public class TestConsole {
 		m[0]="--datasource";
 		m[1]="d:/nba/sta";
 		con.execute(System.out, m);
-//		con.execute(System.out, "-player -high -n 10 -sort frequency".split("\\s"));
-//		con.execute(System.out, "-player -hot assist -n 5".split("\\s"));
-//		con.execute(System.out, "-player -king score -season".split("\\s"));
-//		con.execute(System.out, "-player -avg -n 5 -filter position.F".split("\\s"));
-//		con.execute(System.out, "-player -total -all -n 10 -sort shot".split("\\s"));
-//		con.execute(System.out, "-team -hot assist -n 4".split("\\s"));
-//		con.execute(System.out, "-team -total -all -n 4".split("\\s"));
-//		con.execute(System.out, "-team -all -total".split("\\s"));
+		con.execute(System.out, "-player -high -n 10 -sort frequency".split("\\s"));
+		con.execute(System.out, "-player -hot assist -n 5".split("\\s"));
+		con.execute(System.out, "-player -king score -season".split("\\s"));
+		con.execute(System.out, "-player -avg -n 5 -filter position.F".split("\\s"));
+		con.execute(System.out, "-player -total -all -n 10 -sort shot".split("\\s"));
+		con.execute(System.out, "-team -hot assist -n 4".split("\\s"));
+		con.execute(System.out, "-team -total -all -n 4".split("\\s"));
+		con.execute(System.out, "-team -all -total".split("\\s"));
 		con.execute(System.out, "-player -all -n 50 -total".split("\\s"));
-//		con.execute(System.out, "-player -all -n 50 -total".split("\\s"));
+		con.execute(System.out, "-player -all -n 50 -total".split("\\s"));
 	}
 }
