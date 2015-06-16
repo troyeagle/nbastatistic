337c3bcd6b7bfadcd3a99aa377cd763ecd285b9c
troyeagle <979206913@qq.com>
2015-06-16 22:26:05 +0800

    修正日期查询

--- src/test/java/njuse/ffff/dataGetter/DataReaderTest.java
+++ src/test/java/njuse/ffff/dataGetter/DataReaderTest.java
@@ -15,24 +15,24 @@ import njuse.ffff.sqlpo.TeamInfo;
 public class DataReaderTest {
 	public void test(){
 		NewDataReaderService serv = new DataReader();
-		List<PlayerInMatchFull> leadPlayerForDay = serv.getLeadPlayerForDay(Date.valueOf("2014-11-11"),"points");
-		List<PlayerInMatchFull> leadPlayerForSeason = serv.getLeadPlayerForSeason("14-15", "points");
-		List<TeamAverage> leadTeamForSeason = serv.getLeadTeamForSeason("14-15", "points");
+//		List<PlayerInMatchFull> leadPlayerForDay = serv.getLeadPlayerForDay(Date.valueOf("2014-11-11"),"points");
+//		List<PlayerInMatchFull> leadPlayerForSeason = serv.getLeadPlayerForSeason("14-15", "points");
+//		List<TeamAverage> leadTeamForSeason = serv.getLeadTeamForSeason("14-15", "points");
 		List<MatchInfo> matchInPeriod = serv.getMatchInPeriod(Date.valueOf("2014-11-11"), Date.valueOf("2014-11-30"));
-		PlayerInfo playerInfo = serv.getPlayerInfo("Kobe Bryant");
-		List<PlayerInfo> playerInfoAll = serv.getPlayerInfoAll("playing");
-		List<PlayerInMatchFull> playerInMatch = serv.getPlayerInMatch("198806070LAL");
-		PlayerShooting playershooting = serv.getPlayerShooting("jamesle01", "2014-15");
-		List<PlayerInfo> playersInTeam = serv.getPlayersInTeam("LAL", "14-15");
-		List<PlayerInMatchFull> playerStatsAll = serv.getPlayersStatsAll("2014-15", "per_game");
-		List<PlayerInMatchFull> playerStats = serv.getPlayerStats("jamesle01", "14-15");
-		PlayerInMatchFull playerStatsSingle = serv.getPlayerStatsSingle("jamesle01", "2014-15", "per_game");
-		TeamAverageAdv teamAverageAdv= serv.getTeamAverageAdv("LAL", "2014-15");
-		TeamAverage teamAverage = serv.getTeamAverageSingle("LAL", "2014-15", "total");//不能加s，
-		List<TeamAverage> teamAverages = serv.getTeamAverages("2014-15");
-		List<TeamInfo> teamInfoAll = serv.getTeamInfoAll();
-		List<PlayerInMatchFull> teamStatBySeason = serv.getTeamStatBySeason("LAL", "14-15");
-		PlayerInMatchFull teamStatSingle = serv.getTeamStatSingle("LAL", Date.valueOf("2012-10-30"));
+//		PlayerInfo playerInfo = serv.getPlayerInfo("Kobe Bryant");
+//		List<PlayerInfo> playerInfoAll = serv.getPlayerInfoAll("playing");
+//		List<PlayerInMatchFull> playerInMatch = serv.getPlayerInMatch("198806070LAL");
+//		PlayerShooting playershooting = serv.getPlayerShooting("jamesle01", "2014-15");
+//		List<PlayerInfo> playersInTeam = serv.getPlayersInTeam("LAL", "14-15");
+//		List<PlayerInMatchFull> playerStatsAll = serv.getPlayersStatsAll("2014-15", "per_game");
+//		List<PlayerInMatchFull> playerStats = serv.getPlayerStats("jamesle01", "14-15");
+//		PlayerInMatchFull playerStatsSingle = serv.getPlayerStatsSingle("jamesle01", "2014-15", "per_game");
+//		TeamAverageAdv teamAverageAdv= serv.getTeamAverageAdv("LAL", "2014-15");
+//		TeamAverage teamAverage = serv.getTeamAverageSingle("LAL", "2014-15", "total");//不能加s，
+//		List<TeamAverage> teamAverages = serv.getTeamAverages("2014-15");
+//		List<TeamInfo> teamInfoAll = serv.getTeamInfoAll();
+//		List<PlayerInMatchFull> teamStatBySeason = serv.getTeamStatBySeason("LAL", "14-15");
+//		PlayerInMatchFull teamStatSingle = serv.getTeamStatSingle("LAL", Date.valueOf("2012-10-30"));
 		System.out.println();
 	}
 	public static void main(String args[]){
